447f64f75b0e045ad37b110e151c4f59e121c670
kidkio <am030927@naver.com>
2023-03-22 21:42:42 +0900

    feat: submit page(Load Photo, Take Photo)

--- src/navigation/navigations/UploadingPage.tsx
+++ src/navigation/navigations/UploadingPage.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-  Modal,
-  Pressable,
-} from 'react-native';
+import {ScrollView, StyleSheet, Text, View, Alert} from 'react-native';
+import styled from 'styled-components/native';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 import {colors} from '@/style/theme';
+
+const Photo = styled.Image`
+  width: 200px;
+  height: 200px;
+  border-radius: 8px;
+`;
+const ImagePickerButton = styled.TouchableOpacity`
+  border-width: 1px;
+  border-radius: 8px;
+  border-color: #cccccc;
+  padding: 8px 32px;
+  margin-top: 16px;
+`;
+const Label = styled.Text``;
 
 const style = StyleSheet.create({
   image: {
@@ -25,11 +34,45 @@ const viewStyle = {
 };
 
 export default function SubmitPage() {
+  const [imageSource, setImageSource] = useState<string | undefined>(undefined);
+
+  const showImagePicker = (): void => {
+    launchImageLibrary({mediaType: 'photo'}, response => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.errorCode) {
+        console.log('ImagePicker Error: ', response.errorCode);
+      } else {
+        // You can also display the image using data:
+        // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+        setImageSource(response.assets.uri);
+      }
+    });
+  };
+
+  const showCamera = (): void => {
+    launchCamera({mediaType: 'photo'}, response => {
+      if (response.errorCode) {
+        console.log('LaunchCamera Error: ', response.errorCode);
+      } else {
+        setImageSource(response.assets.uri);
+      }
+    });
+  };
+
   return (
     <SafeAreaView style={viewStyle}>
       <ScrollView>
         <View>
-          <Text>Upload Page</Text>
+          {imageSource && <Photo source={{uri: imageSource}} />}
+          <ImagePickerButton onPress={showImagePicker}>
+            <Label>Load Photo</Label>
+          </ImagePickerButton>
+          <ImagePickerButton onPress={showCamera}>
+            <Label>Take Photo</Label>
+          </ImagePickerButton>
         </View>
       </ScrollView>
     </SafeAreaView>
